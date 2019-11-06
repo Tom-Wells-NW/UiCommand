af6e7d75cca76854c710b229d6587042300b71e8
Tom Wells <twells@pacstar.com>
2019-11-05 16:00:58 -0800

    Code clean-up: removed unused using statements

--- Factories/DeviceViewModelFactory.cs
+++ Factories/DeviceViewModelFactory.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UiCommand.Models;
+﻿using UiCommand.Models;
 using UiCommand.ViewModels;
 
 namespace UiCommand.Factories
